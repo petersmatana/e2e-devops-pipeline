184ddbc9b795c8b3a74dac8759e756c28dd1025d
smonty <smonty@smonty--MacBook-Air.local>
2026-04-18 23:22:30 +0200

    start

---
+++ java-spring-boot-sample/src/main/java/com/example/java_spring_boot_sample/JavaSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootSampleApplication.class, args);
+	}
+
+}
---
+++ java-spring-boot-sample/src/test/java/com/example/java_spring_boot_sample/JavaSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
